23afefbca4644ce98de238c4c9abced29de248d2
SivakumarMurugesan1 <104417652+SivakumarMurugesan1@users.noreply.github.com>
2022-05-04 16:12:33 +0530

    Add files via upload
    stockprice-service

---
+++ stockprice-service/src/main/java/com/stock/stockprice/StockpriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stockprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpriceServiceApplication.class, args);
+	}
+
+}
---
+++ stockprice-service/src/test/java/com/stock/stockprice/StockpriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.stockprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
